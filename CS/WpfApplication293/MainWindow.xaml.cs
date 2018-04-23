f0491cd6382a230e88fa005aa2d112f7a59c1790
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication293/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Diagram.Core;
+using DevExpress.Xpf.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication293
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitCustomDiagramShapes();
+        }
+
+        void InitCustomDiagramShapes()
+        {
+            ResourceDictionary customShapesDictionary = new ResourceDictionary() { Source = new Uri("CustomShapes.xaml", UriKind.Relative) };
+            DiagramToolboxRegistrator.RegisterShapes("MyShapes", () => "Custom Shapes", customShapesDictionary, shapeName => shapeName);
+            diagramControl.DefaultStencils.Add("MyShapes");
+        }
+    }
+}
