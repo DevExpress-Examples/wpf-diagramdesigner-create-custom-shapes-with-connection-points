144e08a72177086a5afba10e553ee63335517229
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to create custom shapes with custom connection points for DiagramDesignerControl
+
+
+<p>The Diagram control supports a special language for defining shapes. The main element that contains shape description is <em>ShapeTemplate</em>. This element describes a shape contour and may contain several segments
+
+* <em>Start</em>. Specifies the start point
+* <em>Line</em>. Defines a line with start and end points
+* <em>Arc</em>. Defines an arc with start and end points<br><br>To specify connection points, use the <em>ShapeTemplate.ConnectionPoints</em> property.<br>To register custom shapes, create a stencil with the <em>DiagramStencil.Create</em> method and pass it to the <em>DiagramToolboxRegistrator.RegisterStencil</em> method.</p>
+
+<br/>
+
+
---
+++ VB/WpfApplication293/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports DevExpress.Diagram.Core
+Imports DevExpress.Xpf.Diagram
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace WpfApplication293
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            InitCustomDiagramShapes()
+        End Sub
+
+        Private Sub InitCustomDiagramShapes()
+            Dim customShapesDictionary As New ResourceDictionary() With {.Source = New Uri("CustomShapes.xaml", UriKind.Relative)}
+            DiagramToolboxRegistrator.RegisterShapes("MyShapes", Function() "Custom Shapes", customShapesDictionary, Function(shapeName) shapeName)
+            diagramControl.SelectedStencils.Add("MyShapes")
+        End Sub
+    End Class
+End Namespace
