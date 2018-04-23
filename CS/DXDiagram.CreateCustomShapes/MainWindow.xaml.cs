0e30b1e2128d402ff272ad89e481c7272cb4f9b9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXDiagram.CreateCustomShapes/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Diagram.Core;
+using DevExpress.Xpf.Core;
+
+namespace DXDiagram.CreateCustomShapes {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            InitCustomDiagramShapes();
+        }
+        void InitCustomDiagramShapes() {
+            ResourceDictionary customShapesDictionary = new ResourceDictionary() { Source = new Uri("CustomShapes.xaml", UriKind.Relative) };
+            var stencil = DiagramStencil.Create("MyShapes", "Custom Shapes", customShapesDictionary, shapeName => shapeName);
+            DiagramToolboxRegistrator.RegisterStencil(stencil);
+            diagramControl.SelectedStencils.Add("MyShapes");
+        }
+    }
+}
---
+++ VB/DXDiagram.CreateCustomShapes/MainWindow.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Windows
+Imports DevExpress.Diagram.Core
+Imports DevExpress.Xpf.Core
+
+Namespace DXDiagram.CreateCustomShapes
+
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            InitCustomDiagramShapes()
+        End Sub
+        Private Sub InitCustomDiagramShapes()
+            Dim customShapesDictionary As New ResourceDictionary() With {.Source = New Uri("CustomShapes.xaml", UriKind.Relative)}
+            Dim stencil = DiagramStencil.Create("MyShapes", "Custom Shapes", customShapesDictionary, Function(shapeName) shapeName)
+            DiagramToolboxRegistrator.RegisterStencil(stencil)
+            diagramControl.SelectedStencils.Add("MyShapes")
+        End Sub
+    End Class
+End Namespace
