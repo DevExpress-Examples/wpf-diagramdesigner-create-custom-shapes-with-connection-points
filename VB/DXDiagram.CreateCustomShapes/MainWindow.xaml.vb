4686315e1fc889d7d1bf4df9d1d5108e4459ed9f
DevExpressExampleBot <fakeEmail>
2019-05-29 14:17:52 +0300

    Source auto update [skip ci]

--- VB/DXDiagram.CreateCustomShapes/MainWindow.xaml.vb
+++ VB/DXDiagram.CreateCustomShapes/MainWindow.xaml.vb
@@ -5,18 +5,18 @@ Imports DevExpress.Xpf.Core
 
 Namespace DXDiagram.CreateCustomShapes
 
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            InitCustomDiagramShapes()
-        End Sub
-        Private Sub InitCustomDiagramShapes()
-            Dim customShapesDictionary As New ResourceDictionary() With {.Source = New Uri("CustomShapes.xaml", UriKind.Relative)}
-            Dim stencil = DiagramStencil.Create("MyShapes", "Custom Shapes", customShapesDictionary, Function(shapeName) shapeName)
-            DiagramToolboxRegistrator.RegisterStencil(stencil)
-            diagramControl.SelectedStencils.Add("MyShapes")
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			InitCustomDiagramShapes()
+		End Sub
+		Private Sub InitCustomDiagramShapes()
+			Dim customShapesDictionary As New ResourceDictionary() With {.Source = New Uri("CustomShapes.xaml", UriKind.Relative)}
+			Dim stencil = DiagramStencil.Create("MyShapes", "Custom Shapes", customShapesDictionary, Function(shapeName) shapeName)
+			DiagramToolboxRegistrator.RegisterStencil(stencil)
+			diagramControl.SelectedStencils.Add("MyShapes")
+		End Sub
+	End Class
 End Namespace
